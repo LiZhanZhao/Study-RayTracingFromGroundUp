d8914fd5cc6de73374f61fdb90ae886015778a72
lzz <120274270@qq.com>
2016-10-08 18:05:58 +0800

    add main class

---
+++ Src/SingleSphere.cpp
@@ -0,0 +1,44 @@
+// 	Copyright (C) Kevin Suffern 2000-2007.
+//	This C++ code is for non-commercial purposes only.
+//	This C++ code is licensed under the GNU General Public License Version 2.
+//	See the file COPYING.txt for the full license.
+
+
+#include "SingleSphere.h"
+#include "World.h"
+#include "ShadeRec.h"
+
+// -------------------------------------------------------------------- default constructor
+
+SingleSphere::SingleSphere(void)
+	: Tracer()
+{}
+
+
+// -------------------------------------------------------------------- constructor
+		
+SingleSphere::SingleSphere(World* _worldPtr)
+	: Tracer(_worldPtr)
+{}
+
+
+// -------------------------------------------------------------------- destructor
+
+SingleSphere::~SingleSphere(void) {}
+
+
+// -------------------------------------------------------------------- trace_ray
+
+RGBColor	
+SingleSphere::trace_ray(const Ray& ray) const {
+	ShadeRec	sr(*world_ptr); 	// not used
+	double    	t;  				// not used
+	
+	if (world_ptr->sphere.hit(ray, t, sr))		
+		return (red);  
+	else
+		return (black);   
+}
+
+
+

8e6db599b594ae623c674c2b16528deaea2321e4
lzz <120274270@qq.com>
2016-11-08 15:02:20 +0800

    Add Phong material shadow function

--- Src/Phong.cpp
+++ Src/Phong.cpp
@@ -79,6 +79,38 @@ RGBColor Phong::shade(ShadeRec& sr) {
 	int 		num_lights = sr.w.lights.size();
 
 	for (int j = 0; j < num_lights; j++) {
+		// wi is negetive the real light direction
+		Vector3D wi = sr.w.lights[j]->get_direction(sr);
+		float ndotwi = sr.normal * wi;
+
+		if (ndotwi > 0.0)
+		{
+
+			bool in_shadows = false;
+
+			if (sr.w.lights[j]->casts_shadows()){
+				Ray shadowRay(sr.hit_point, wi);
+				in_shadows = sr.w.lights[j]->in_shadow(shadowRay, sr);
+			}
+
+			if (in_shadows){
+				L += (diffuse_brdf->f(sr, wo, wi) + specular_brdf->f(sr, wo, wi))
+					* sr.w.lights[j]->L(sr) * ndotwi;
+			}
+		}
+	}
+
+	return (L);
+}
+
+/*
+RGBColor Phong::shade(ShadeRec& sr) {
+	// ray is reflect to eye, so need negetive sr.ray.d
+	Vector3D 	wo = -sr.ray.d;
+	RGBColor 	L = ambient_brdf->rho(sr, wo) * sr.w.ambient_ptr->L(sr);
+	int 		num_lights = sr.w.lights.size();
+
+	for (int j = 0; j < num_lights; j++) {
 		Vector3D wi = sr.w.lights[j]->get_direction(sr);
 		float ndotwi = sr.normal * wi;
 
@@ -92,3 +124,4 @@ RGBColor Phong::shade(ShadeRec& sr) {
 
 	return (L);
 }
+*/

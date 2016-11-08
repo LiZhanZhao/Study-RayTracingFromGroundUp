cbf10959b1163c312cc7b019d1eb530f18f6d589
lzz <120274270@qq.com>
2016-11-08 11:29:48 +0800

    add phong material setproperty funtion

--- Src/Phong.h
+++ Src/Phong.h
@@ -20,6 +20,20 @@ class Phong : public Material {
 
 		virtual RGBColor shade(ShadeRec& sr);
 
+		void set_ka(const float k);
+
+		void set_kd(const float k);
+
+		void set_cd(const RGBColor c);
+
+		void set_cd(const float r, const float g, const float b);
+
+		void set_cd(const float c);
+
+		void set_ks(const float k);
+
+		void set_exp(const float exp);
+
 	private:
 
 		Lambertian*		ambient_brdf;
@@ -28,6 +42,35 @@ class Phong : public Material {
 
 };
 
+inline void Phong::set_ka(const float ka) {
+	ambient_brdf->set_kd(ka);
+}
 
+inline void Phong::set_kd(const float kd) {
+	diffuse_brdf->set_kd(kd);
+}
+
+inline void Phong::set_cd(const RGBColor c) {
+	ambient_brdf->set_cd(c);
+	diffuse_brdf->set_cd(c);
+}
+
+inline void Phong::set_cd(const float r, const float g, const float b) {
+	ambient_brdf->set_cd(r, g, b);
+	diffuse_brdf->set_cd(r, g, b);
+}
+
+inline void Phong::set_cd(const float c) {
+	ambient_brdf->set_cd(c);
+	diffuse_brdf->set_cd(c);
+}
+
+inline void Phong::set_ks(const float k){
+	specular_brdf->set_ks(k);
+}
+
+inline void Phong::set_exp(const float exp){
+	specular_brdf->set_exp(exp);
+}
 
 #endif

5366540cdb889fc696178e5a16e655fe1bdf91f5
lzz <120274270@qq.com>
2016-11-08 14:19:00 +0800

    delete useless code

--- Src/BuildFigure.cpp
+++ Src/BuildFigure.cpp
@@ -51,7 +51,7 @@ void World::build(void) {
 	light_ptr2->scale_radiance(2.0);
 	add_light(light_ptr2);
 
-
+	
 	Phong* phong_ptr1 = new Phong;
 	phong_ptr1->set_ka(0.25);
 	phong_ptr1->set_kd(0.75);
@@ -65,21 +65,20 @@ void World::build(void) {
 	phong_ptr2->set_cd(0.75, 0.25, 0);   	// orange
 	phong_ptr2->set_ks(0.25);
 	phong_ptr2->set_exp(500);
+	
+	/*
+	Matte* phong_ptr1 = new Matte;
+	phong_ptr1->set_ka(0.25);
+	phong_ptr1->set_kd(0.75);
+	phong_ptr1->set_cd(0.75, 0.75, 0);  	// dark yellow
 
-	//Phong* phong_ptr3 = new Phong;
-	//phong_ptr3->set_ka(0.4);
-	//phong_ptr3->set_kd(0.75);
-	//phong_ptr3->set_cd(1, 0.5, 1);			// mauve
-	//phong_ptr3->set_ks(0.25);
-	//phong_ptr3->set_exp(4);
+	Matte* phong_ptr2 = new Matte;
+	phong_ptr2->set_ka(0.45);
+	phong_ptr2->set_kd(0.75);
+	phong_ptr2->set_cd(0.75, 0.25, 0);   	// orange
+	*/
 
-	//Phong* phong_ptr4 = new Phong;
-	//phong_ptr4->set_ka(0.15);
-	//phong_ptr4->set_kd(0.5);
-	//phong_ptr4->set_cd(0.75, 1.0, 0.75);   	// light green
-	//phong_ptr4->set_ks(0.5);
-	//phong_ptr4->set_exp(3);
-
+	
 	Matte* matte_ptr5 = new Matte;
 	matte_ptr5->set_ka(0.20);
 	matte_ptr5->set_kd(0.97);
@@ -95,21 +94,6 @@ void World::build(void) {
 	Sphere* sphere_ptr2 = new Sphere(Point3D(-0.7, 1, 4.2), 2);
 	sphere_ptr2->set_material(phong_ptr2);
 	add_object(sphere_ptr2);
-
-	// cylinder 
-
-	/*float bottom = 0.0;
-	float top = 8.5;
-	float radius = 2.2;
-	SolidCylinder* cylinder_ptr = new SolidCylinder(bottom, top, radius);
-	cylinder_ptr->set_material(phong_ptr3);
-	add_object(cylinder_ptr);*/
-
-	// box
-
-	/*Box* box_ptr = new Box(Point3D(-3.5, 0, -11), Point3D(-2.5, 6, 6.5));
-	box_ptr->set_material(phong_ptr4);
-	add_object(box_ptr);*/
 
 	// ground plane
 

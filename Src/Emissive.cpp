04e315963e0ecf9e59be3024311a1cd4310639d1
lzz <120274270@qq.com>
2016-11-10 22:49:14 +0800

    add Emissive material class

---
+++ Src/Emissive.cpp
@@ -0,0 +1,43 @@
+#include "Emissive.h"
+
+Emissive::Emissive(void) : Material(), ls(1), ce(1.0)
+{}
+
+Emissive::Emissive(const Emissive& m) : Material(m), ls(m.ls), ce(m.ce)
+{}
+
+Material* Emissive::clone(void) const {
+	return (new Emissive(*this));
+}
+
+Emissive& Emissive::operator= (const Emissive& rhs){
+	if (this == &rhs)
+		return (*this);
+
+	Material::operator=(rhs);
+	ls = rhs.ls;
+	ce = rhs.ce;
+	return (*this);
+}
+
+Emissive::~Emissive(void){}
+
+RGBColor Emissive::area_light_shade(ShadeRec& sr) {
+	// when the incoming ray is on the same side of the object surface as the normal
+	if (-sr.normal * sr.ray.d > 0.0)
+		return (ls * ce);
+	else
+		return (black);
+}
+
+RGBColor Emissive::shade(ShadeRec& sr){
+	// when the incoming ray is on the same side of the object surface as the normal
+	if (-sr.normal * sr.ray.d > 0.0)
+		return (ls * ce);
+	else
+		return (black);
+}
+
+RGBColor Emissive::get_Le(ShadeRec& sr) const{
+	return ls * ce;
+}
---
+++ Src/Emissive.h
@@ -0,0 +1,48 @@
+#ifndef __EMISSIVE__
+#define __EMISSIVE__
+
+#include "Material.h"
+#include "Lambertian.h"
+
+
+class Emissive : public Material {
+private:
+
+	float		ls;		// radiance scaling factor						 
+	RGBColor 	ce;		// color
+
+public:
+
+	// constructors, set functions, etc ...
+	Emissive(void);
+
+	Emissive(const Emissive& m);
+
+	virtual Material* clone(void) const;
+
+	Emissive& operator= (const Emissive& rhs);
+
+	~Emissive(void);
+
+	void scale_radiance(const float _ls);
+
+	void set_ce(const float r, const float g, const float b);
+
+	virtual RGBColor get_Le(ShadeRec& sr) const;
+
+	virtual RGBColor shade(ShadeRec& sr);
+
+	virtual RGBColor area_light_shade(ShadeRec& sr);
+};
+
+inline void Emissive::scale_radiance(const float _ls){
+	ls = _ls;
+}
+
+inline void Emissive::set_ce(const float r, const float g, const float b){
+	ce.r = r;
+	ce.g = g;
+	ce.b = b;
+}
+
+#endif

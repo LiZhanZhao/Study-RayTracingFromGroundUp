60321944dabe01c617821d50fa27c03862f614c2
lzz <120274270@qq.com>
2016-11-28 23:14:38 +0800

    test two triangle ply file and ready find bug

--- Src/BuildFigure.cpp
+++ Src/BuildFigure.cpp
@@ -52,53 +52,109 @@ void World::build(void) {
 	vp.set_vres(vpHeight);
 	vp.set_samples(num_samples);
 
-	background_color = RGBColor(0.9);
-
 	tracer_ptr = new RayCast(this);
 
+	background_color = black;
+
 	Pinhole* pinhole_ptr = new Pinhole;
-	pinhole_ptr->set_eye(10, 12, 18);
-	pinhole_ptr->set_lookat(0);
-	pinhole_ptr->set_view_distance(4000);
+	pinhole_ptr->set_eye(100, 50, 90);
+	pinhole_ptr->set_lookat(0, -0.5, 0);
+	pinhole_ptr->set_view_distance(16000);
 	pinhole_ptr->compute_uvw();
 	set_camera(pinhole_ptr);
 
+	Directional* directional_ptr = new Directional;
+	directional_ptr->set_direction(0.75, 1, -0.15);
+	directional_ptr->scale_radiance(4.5);
+	directional_ptr->set_shadows(true);
+	add_light(directional_ptr);
 
-	Directional* light_ptr = new Directional;
-	light_ptr->set_direction(20, 25, 5);
-	light_ptr->scale_radiance(3.0);
-	light_ptr->set_shadows(false);
-	add_light(light_ptr);
+	Matte* matte_ptr1 = new Matte;
+	matte_ptr1->set_ka(0.1);
+	matte_ptr1->set_kd(0.75);
+	matte_ptr1->set_cd(0.1, 0.5, 1.0);
 
-
-	Phong* phong_ptr = new Phong;
-	phong_ptr->set_ka(0.25);
-	phong_ptr->set_kd(0.75);
-	phong_ptr->set_cd(0.5);
-	phong_ptr->set_cd(0.2, 0.5, 0.4);
-	phong_ptr->set_ks(0.2);
-	phong_ptr->set_exp(20.0);
-
-	// 360 must be divisible by num_horizontal_steps
-	// 180 must be divisible by num_vertical_steps
-
-	//int num_horizontal_steps = 3;    	// for Figure 23.3(a)
-	//int num_vertical_steps = 2;
-
-	//	int num_horizontal_steps = 4;		// for Figure 23.3(b)
-	//	int num_vertical_steps = 2;
-
-	//	int num_horizontal_steps = 10;		// for Figure 23.3(c)
-	//	int num_vertical_steps = 5;
-
-		int num_horizontal_steps = 100;		// for Figure 23.3(d)
-		int num_vertical_steps = 50;
-
-	Grid* grid_ptr = new Grid;
-	grid_ptr->tessellate_smooth_sphere(num_horizontal_steps, num_vertical_steps);
-	grid_ptr->set_material(phong_ptr);
+	char* file_name = "Ply/TwoTriangles.ply";
+	Grid* grid_ptr = new Grid(new Mesh);
+	grid_ptr->read_flat_triangles(file_name);		// for Figure 23.7(a)
+	//	grid_ptr->read_smooth_triangles(file_name);		// for Figure 23.7(b)
+	grid_ptr->set_material(matte_ptr1);
 	grid_ptr->setup_cells();
 	add_object(grid_ptr);
+
+	Matte* matte_ptr2 = new Matte;
+	matte_ptr2->set_cd(1, 0.9, 0.6);
+	matte_ptr2->set_ka(0.25);
+	matte_ptr2->set_kd(0.4);
+
+	Plane* plane_ptr1 = new Plane(Point3D(0, -2.0, 0), Normal(0, 1, 0));
+	plane_ptr1->set_material(matte_ptr2);
+	add_object(plane_ptr1);
+
+
+
+
+	//int vpWidth = 400;
+	//int vpHeight = 400;
+	//int num_samples = 16;
+
+	//imageWidth = vpWidth;
+	//imageHeight = vpHeight; 
+	//imageBuffer = new RGBColor[imageWidth * imageHeight];
+
+	//vp.set_hres(vpWidth);
+	//vp.set_vres(vpHeight);
+	//vp.set_samples(num_samples);
+
+	//background_color = RGBColor(0.9);
+
+	//tracer_ptr = new RayCast(this);
+
+	//Pinhole* pinhole_ptr = new Pinhole;
+	//pinhole_ptr->set_eye(10, 12, 18);
+	//pinhole_ptr->set_lookat(0);
+	//pinhole_ptr->set_view_distance(4000);
+	//pinhole_ptr->compute_uvw();
+	//set_camera(pinhole_ptr);
+
+
+	//Directional* light_ptr = new Directional;
+	//light_ptr->set_direction(20, 25, 5);
+	//light_ptr->scale_radiance(3.0);
+	//light_ptr->set_shadows(false);
+	//add_light(light_ptr);
+
+
+	//Phong* phong_ptr = new Phong;
+	//phong_ptr->set_ka(0.25);
+	//phong_ptr->set_kd(0.75);
+	//phong_ptr->set_cd(0.5);
+	//phong_ptr->set_cd(0.2, 0.5, 0.4);
+	//phong_ptr->set_ks(0.2);
+	//phong_ptr->set_exp(20.0);
+
+	//// 360 must be divisible by num_horizontal_steps
+	//// 180 must be divisible by num_vertical_steps
+
+	////int num_horizontal_steps = 3;    	// for Figure 23.3(a)
+	////int num_vertical_steps = 2;
+
+	////	int num_horizontal_steps = 4;		// for Figure 23.3(b)
+	////	int num_vertical_steps = 2;
+
+	////	int num_horizontal_steps = 10;		// for Figure 23.3(c)
+	////	int num_vertical_steps = 5;
+
+	//	int num_horizontal_steps = 100;		// for Figure 23.3(d)
+	//	int num_vertical_steps = 50;
+
+	//Grid* grid_ptr = new Grid;
+	//grid_ptr->tessellate_smooth_sphere(num_horizontal_steps, num_vertical_steps);
+	//grid_ptr->set_material(phong_ptr);
+	//grid_ptr->setup_cells();
+	//add_object(grid_ptr);
+
+
 
 
 

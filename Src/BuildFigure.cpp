cd7f6b165572b05fa0fbd3b241dd2846e9e1864e
lzz <120274270@qq.com>
2016-12-28 21:43:27 +0800

    add Dielectric image configure then ready fixed bug

--- Src/BuildFigure.cpp
+++ Src/BuildFigure.cpp
@@ -12,6 +12,7 @@
 #include "Reflective.h"
 #include "GlossyReflector.h"
 #include "Transparent.h"
+#include "Dielectric.h"
 
 // Light
 #include "Ambient.h"
@@ -49,16 +50,15 @@ void World::build(void) {
 	int vpWidth = 600;
 	int vpHeight = 600;
 	int num_samples = 16;
+
 	imageWidth = vpWidth;
 	imageHeight = vpHeight;
 	imageBuffer = new RGBColor[imageWidth * imageHeight];
 
-	vp.set_hres(vpWidth);
-	vp.set_vres(vpHeight);
+	vp.set_hres(imageWidth);
+	vp.set_vres(imageHeight);
 	vp.set_samples(num_samples);
-	vp.set_max_depth(2);			// for Figure 27.13(a)
-	//	vp.set_max_depth(4);			// for Figure 27.13(b)
-	//	vp.set_max_depth(5);			// for Figure 27.13(c)	
+	vp.set_max_depth(5);
 
 	background_color = RGBColor(0.0, 0.3, 0.25);
 
@@ -82,7 +82,6 @@ void World::build(void) {
 	light_ptr1->set_location(40, 50, 0);
 	light_ptr1->scale_radiance(4.5);
 	light_ptr1->set_shadows(true);
-	//light_ptr1->set_shadows(false);
 	add_light(light_ptr1);
 
 
@@ -92,7 +91,6 @@ void World::build(void) {
 	light_ptr2->set_location(-10, 20, 10);
 	light_ptr2->scale_radiance(4.5);
 	light_ptr2->set_shadows(true);
-	//light_ptr2->set_shadows(false);
 	add_light(light_ptr2);
 
 
@@ -102,24 +100,22 @@ void World::build(void) {
 	light_ptr3->set_direction(-1, 0, 0);
 	light_ptr3->scale_radiance(4.5);
 	light_ptr3->set_shadows(true);
-	//light_ptr3->set_shadows(false);
 	add_light(light_ptr3);
 
 
 	// transparent sphere
 
-	Transparent* glass_ptr = new Transparent;
-	glass_ptr->set_ks(0.2);
-	glass_ptr->set_exp(2000.0);
-	//glass_ptr->set_ior(1.0);
-	//glass_ptr->set_ior(1.5);
-	glass_ptr->set_ior(0.75);
-	glass_ptr->set_kr(0.1);
-	//glass_ptr->set_kr(1);
-	glass_ptr->set_kt(0.9);
+	Dielectric* dielectic_ptr = new Dielectric;
+	dielectic_ptr->set_ks(0.2);
+	dielectic_ptr->set_exp(2000.0);
+	dielectic_ptr->set_eta_in(1.5);		// for Figure 28.5(a)
+	//	dielectic_ptr->set_eta_in(0.75);	// for Figure 28.5(b)
+	dielectic_ptr->set_eta_out(1.0);
+	dielectic_ptr->set_cf_in(white);
+	dielectic_ptr->set_cf_out(white);
 
 	Sphere* sphere_ptr1 = new Sphere(Point3D(0.0, 4.5, 0.0), 3.0);
-	sphere_ptr1->set_material(glass_ptr);
+	sphere_ptr1->set_material(dielectic_ptr);
 	add_object(sphere_ptr1);
 
 
@@ -138,17 +134,20 @@ void World::build(void) {
 	add_object(sphere_ptr2);
 
 
-	//Checker3D* checker_ptr = new Checker3D;
-	//checker_ptr->set_size(4);
-	//checker_ptr->set_color1(0.75);
-	//checker_ptr->set_color2(white);
+	/*Checker3D* checker_ptr = new Checker3D;
+	checker_ptr->set_size(4);
+	checker_ptr->set_color1(0.75);
+	checker_ptr->set_color2(white);*/
 
-	//SV_Matte* sv_matte_ptr = new SV_Matte;
+	/*SV_Matte* sv_matte_ptr = new SV_Matte;
+	sv_matte_ptr->set_ka(0.5);
+	sv_matte_ptr->set_kd(0.35);
+	sv_matte_ptr->set_cd(checker_ptr);*/
+
 	Matte* sv_matte_ptr = new Matte;
 	sv_matte_ptr->set_ka(0.5);
 	sv_matte_ptr->set_kd(0.35);
 	sv_matte_ptr->set_cd(0.37, 0.59, 0.2);
-	//sv_matte_ptr->set_cd(checker_ptr);
 
 	// rectangle
 
@@ -166,12 +165,9 @@ void World::build(void) {
 
 
 
-
-
-
-	//int vpWidth = 300;
-	//int vpHeight = 300;
-	//int num_samples = 1;
+	//int vpWidth = 600;
+	//int vpHeight = 600;
+	//int num_samples = 16;
 	//imageWidth = vpWidth;
 	//imageHeight = vpHeight;
 	//imageBuffer = new RGBColor[imageWidth * imageHeight];
@@ -179,233 +175,109 @@ void World::build(void) {
 	//vp.set_hres(vpWidth);
 	//vp.set_vres(vpHeight);
 	//vp.set_samples(num_samples);
-	//vp.set_max_depth(0);
+	//vp.set_max_depth(2);			// for Figure 27.13(a)
+	////	vp.set_max_depth(4);			// for Figure 27.13(b)
+	////	vp.set_max_depth(5);			// for Figure 27.13(c)	
 
-	//background_color = black;
+	//background_color = RGBColor(0.0, 0.3, 0.25);
 
-	//tracer_ptr = new GlobalTrace(this);
+	//tracer_ptr = new Whitted(this);
 
 	//Ambient* ambient_ptr = new Ambient;
-	//ambient_ptr->scale_radiance(0.0);
+	//ambient_ptr->scale_radiance(0.25);
 	//set_ambient_light(ambient_ptr);
 
-
 	//Pinhole* pinhole_ptr = new Pinhole;
-	//pinhole_ptr->set_eye(27.6, 27.4, -80.0);
-	//pinhole_ptr->set_lookat(27.6, 27.4, 0.0);
-	//pinhole_ptr->set_view_distance(400);
+	//pinhole_ptr->set_eye(-8, 5.5, 40);
+	//pinhole_ptr->set_lookat(1, 4, 0);
+	//pinhole_ptr->set_view_distance(2400.0);
 	//pinhole_ptr->compute_uvw();
 	//set_camera(pinhole_ptr);
 
 
-	//Point3D p0;
-	//Vector3D a, b;
-	//Normal normal;
+	//// point light 
 
-	//// box dimensions
-
-	//double width = 55.28;   	// x direction
-	//double height = 54.88;  	// y direction
-	//double depth = 55.92;	// z direction
-
-
-	//Emissive* emissive_ptr = new Emissive;
-	//emissive_ptr->set_ce(1.0, 0.73, 0.4);
-	//emissive_ptr->scale_radiance(100.0);
-
-	//p0 = Point3D(21.3, height - 0.001, 22.7);
-	//a = Vector3D(0.0, 0.0, 10.5);
-	//b = Vector3D(13.0, 0.0, 0.0);
-	//normal = Normal(0.0, -1.0, 0.0);
-
-	//Rectangle* light_ptr = new Rectangle(p0, a, b, normal);
-	//light_ptr->set_material(emissive_ptr);
-	//light_ptr->set_sampler(new MultiJittered(num_samples));
-	//light_ptr->set_shadows(false);
-	//add_object(light_ptr);
-
-	//AreaLight* ceiling_light_ptr = new AreaLight;
-	//ceiling_light_ptr->set_object(light_ptr);
-	//ceiling_light_ptr->set_shadows(true);
-	//add_light(ceiling_light_ptr);
+	//PointLight* light_ptr1 = new PointLight;
+	//light_ptr1->set_location(40, 50, 0);
+	//light_ptr1->scale_radiance(4.5);
+	//light_ptr1->set_shadows(true);
+	////light_ptr1->set_shadows(false);
+	//add_light(light_ptr1);
 
 
-	//// left wall
+	//// point light 
 
-	//Matte* matte_ptr1 = new Matte;
-	//matte_ptr1->set_ka(0.0);
-	//matte_ptr1->set_kd(0.6);
-	//matte_ptr1->set_cd(0.57, 0.025, 0.025);	 // red
-	//matte_ptr1->set_sampler(new MultiJittered(num_samples));
-
-	//p0 = Point3D(width, 0.0, 0.0);
-	//a = Vector3D(0.0, 0.0, depth);
-	//b = Vector3D(0.0, height, 0.0);
-	//normal = Normal(-1.0, 0.0, 0.0);
-	//Rectangle* left_wall_ptr = new Rectangle(p0, a, b, normal);
-	//left_wall_ptr->set_material(matte_ptr1);
-	//add_object(left_wall_ptr);
+	//PointLight* light_ptr2 = new PointLight;
+	//light_ptr2->set_location(-10, 20, 10);
+	//light_ptr2->scale_radiance(4.5);
+	//light_ptr2->set_shadows(true);
+	////light_ptr2->set_shadows(false);
+	//add_light(light_ptr2);
 
 
-	//// right wall
+	//// directional light 
 
-	//Matte* matte_ptr2 = new Matte;
-	//matte_ptr2->set_ka(0.0);
-	//matte_ptr2->set_kd(0.6);
-	//matte_ptr2->set_cd(0.37, 0.59, 0.2);	 // green  
-	//matte_ptr2->set_sampler(new MultiJittered(num_samples));
-
-	//p0 = Point3D(0.0, 0.0, 0.0);
-	//a = Vector3D(0.0, 0.0, depth);
-	//b = Vector3D(0.0, height, 0.0);
-	//normal = Normal(1.0, 0.0, 0.0);
-	//Rectangle* right_wall_ptr = new Rectangle(p0, a, b, normal);
-	//right_wall_ptr->set_material(matte_ptr2);
-	//add_object(right_wall_ptr);
+	//Directional* light_ptr3 = new Directional;
+	//light_ptr3->set_direction(-1, 0, 0);
+	//light_ptr3->scale_radiance(4.5);
+	//light_ptr3->set_shadows(true);
+	////light_ptr3->set_shadows(false);
+	//add_light(light_ptr3);
 
 
-	//// back wall
+	//// transparent sphere
 
-	//Matte* matte_ptr3 = new Matte;
-	//matte_ptr3->set_ka(0.0);
-	//matte_ptr3->set_kd(0.6);
-	//matte_ptr3->set_cd(white);
-	//matte_ptr3->set_sampler(new MultiJittered(num_samples));
+	//Transparent* glass_ptr = new Transparent;
+	//glass_ptr->set_ks(0.2);
+	//glass_ptr->set_exp(2000.0);
+	////glass_ptr->set_ior(1.0);
+	////glass_ptr->set_ior(1.5);
+	//glass_ptr->set_ior(0.75);
+	//glass_ptr->set_kr(0.1);
+	////glass_ptr->set_kr(1);
+	//glass_ptr->set_kt(0.9);
 
-	//p0 = Point3D(0.0, 0.0, depth);
-	//a = Vector3D(width, 0.0, 0.0);
-	//b = Vector3D(0.0, height, 0.0);
-	//normal = Normal(0.0, 0.0, -1.0);
-	//Rectangle* back_wall_ptr = new Rectangle(p0, a, b, normal);
-	//back_wall_ptr->set_material(matte_ptr3);
-	//add_object(back_wall_ptr);
-
-
-	//// floor
-
-	//p0 = Point3D(0.0, 0.0, 0.0);
-	//a = Vector3D(0.0, 0.0, depth);
-	//b = Vector3D(width, 0.0, 0.0);
-	//normal = Normal(0.0, 1.0, 0.0);
-	//Rectangle* floor_ptr = new Rectangle(p0, a, b, normal);
-	//floor_ptr->set_material(matte_ptr3);
-	//add_object(floor_ptr);
+	//Sphere* sphere_ptr1 = new Sphere(Point3D(0.0, 4.5, 0.0), 3.0);
+	//sphere_ptr1->set_material(glass_ptr);
+	//add_object(sphere_ptr1);
 
 
-	//// ceiling
+	//// red sphere
 
-	//p0 = Point3D(0.0, height, 0.0);
-	//a = Vector3D(0.0, 0.0, depth);
-	//b = Vector3D(width, 0.0, 0.0);
-	//normal = Normal(0.0, -1.0, 0.0);
-	//Rectangle* ceiling_ptr = new Rectangle(p0, a, b, normal);
-	//ceiling_ptr->set_material(matte_ptr3);
-	//add_object(ceiling_ptr);
+	//Reflective*	reflective_ptr = new Reflective;
+	//reflective_ptr->set_ka(0.3);
+	//reflective_ptr->set_kd(0.3);
+	//reflective_ptr->set_cd(red);
+	//reflective_ptr->set_ks(0.2);
+	//reflective_ptr->set_exp(2000.0);
+	//reflective_ptr->set_kr(0.25);
 
-
-	//// the two boxes defined as 5 rectangles each
-
-	//// short box
-
-	//// top
-
-	//p0 = Point3D(13.0, 16.5, 6.5);
-	//a = Vector3D(-4.8, 0.0, 16.0);
-	//b = Vector3D(16.0, 0.0, 4.9);
-	//normal = Normal(0.0, 1.0, 0.0);
-	//Rectangle* short_top_ptr = new Rectangle(p0, a, b, normal);
-	//short_top_ptr->set_material(matte_ptr3);
-	//add_object(short_top_ptr);
+	//Sphere* sphere_ptr2 = new Sphere(Point3D(4, 4, -6), 3);
+	//sphere_ptr2->set_material(reflective_ptr);
+	//add_object(sphere_ptr2);
 
 
-	//// side 1
+	////Checker3D* checker_ptr = new Checker3D;
+	////checker_ptr->set_size(4);
+	////checker_ptr->set_color1(0.75);
+	////checker_ptr->set_color2(white);
 
-	//p0 = Point3D(13.0, 0.0, 6.5);
-	//a = Vector3D(-4.8, 0.0, 16.0);
-	//b = Vector3D(0.0, 16.5, 0.0);
-	//Rectangle* short_side_ptr1 = new Rectangle(p0, a, b);
-	//short_side_ptr1->set_material(matte_ptr3);
-	//add_object(short_side_ptr1);
+	////SV_Matte* sv_matte_ptr = new SV_Matte;
+	//Matte* sv_matte_ptr = new Matte;
+	//sv_matte_ptr->set_ka(0.5);
+	//sv_matte_ptr->set_kd(0.35);
+	//sv_matte_ptr->set_cd(0.37, 0.59, 0.2);
+	////sv_matte_ptr->set_cd(checker_ptr);
 
+	//// rectangle
 
-	//// side 2
+	//Point3D p0(-20, 0, -100);
+	//Vector3D a(0, 0, 120);
+	//Vector3D b(40, 0, 0);
 
-	//p0 = Point3D(8.2, 0.0, 22.5);
-	//a = Vector3D(15.8, 0.0, 4.7);
-	//Rectangle* short_side_ptr2 = new Rectangle(p0, a, b);
-	//short_side_ptr2->set_material(matte_ptr3);
-	//add_object(short_side_ptr2);
-
-
-	//// side 3
-
-	//p0 = Point3D(24.2, 0.0, 27.4);
-	//a = Vector3D(4.8, 0.0, -16.0);
-	//Rectangle* short_side_ptr3 = new Rectangle(p0, a, b);
-	//short_side_ptr3->set_material(matte_ptr3);
-	//add_object(short_side_ptr3);
-
-
-	//// side 4
-
-	//p0 = Point3D(29.0, 0.0, 11.4);
-	//a = Vector3D(-16.0, 0.0, -4.9);
-	//Rectangle* short_side_ptr4 = new Rectangle(p0, a, b);
-	//short_side_ptr4->set_material(matte_ptr3);
-	//add_object(short_side_ptr4);
-
-
-
-	//// tall box
-
-	//// top
-
-	//p0 = Point3D(42.3, 33.0, 24.7);
-	//a = Vector3D(-15.8, 0.0, 4.9);
-	//b = Vector3D(4.9, 0.0, 15.9);
-	//normal = Normal(0.0, 1.0, 0.0);
-	//Rectangle* tall_top_ptr = new Rectangle(p0, a, b, normal);
-	//tall_top_ptr->set_material(matte_ptr3);
-	//add_object(tall_top_ptr);
-
-
-	//// side 1
-
-	//p0 = Point3D(42.3, 0.0, 24.7);
-	//a = Vector3D(-15.8, 0.0, 4.9);
-	//b = Vector3D(0.0, 33.0, 0.0);
-	//Rectangle* tall_side_ptr1 = new Rectangle(p0, a, b);
-	//tall_side_ptr1->set_material(matte_ptr3);
-	//add_object(tall_side_ptr1);
-
-
-	//// side 2
-
-	//p0 = Point3D(26.5, 0.0, 29.6);
-	//a = Vector3D(4.9, 0.0, 15.9);
-	//Rectangle* tall_side_ptr2 = new Rectangle(p0, a, b);
-	//tall_side_ptr2->set_material(matte_ptr3);
-	//add_object(tall_side_ptr2);
-
-
-	//// side 3
-
-	//p0 = Point3D(31.4, 0.0, 45.5);
-	//a = Vector3D(15.8, 0.0, -4.9);
-	//Rectangle* tall_side_ptr3 = new Rectangle(p0, a, b);
-	//tall_side_ptr3->set_material(matte_ptr3);
-	//add_object(tall_side_ptr3);
-
-
-	//// side 4
-
-	//p0 = Point3D(47.2, 0.0, 40.6);
-	//a = Vector3D(-4.9, 0.0, -15.9);
-	//Rectangle* tall_side_ptr4 = new Rectangle(p0, a, b);
-	//tall_side_ptr4->set_material(matte_ptr3);
-	//add_object(tall_side_ptr4);
-
-
+	//Rectangle* rectangle_ptr = new Rectangle(p0, a, b);
+	//rectangle_ptr->set_material(sv_matte_ptr);
+	//add_object(rectangle_ptr);
 }
 
 

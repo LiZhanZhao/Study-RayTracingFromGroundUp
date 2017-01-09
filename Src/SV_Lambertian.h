eeba8ab51d3efcd26d6342336cde01790cbafd82
lzz <120274270@qq.com>
2017-01-09 08:18:01 +0800

    add Texture and SV_Lambertian material class

---
+++ Src/SV_Lambertian.h
@@ -0,0 +1,37 @@
+#ifndef __SV_LAMBERTIAN__
+#define __SV_LAMBERTIAN__
+
+#include "BRDF.h"
+#include "Texture.h"
+
+class SV_Lambertian : public BRDF {
+public:
+
+	// constructors, etc ...
+	SV_Lambertian(void);
+
+	SV_Lambertian(const SV_Lambertian& lamb);
+
+	SV_Lambertian& operator= (const SV_Lambertian& rhs);
+
+	virtual ~SV_Lambertian(void);
+
+	virtual SV_Lambertian* clone(void) const;
+
+	virtual RGBColor
+		rho(const ShadeRec& sr, const Vector3D& wo) const;
+
+	virtual RGBColor
+		f(const ShadeRec& sr, const Vector3D& wo, const Vector3D& wi) const;
+
+	virtual RGBColor
+		sample_f(const ShadeRec& sr, const Vector3D& wo, Vector3D& wi, float& pdf) const;
+
+private:
+
+	float		kd;
+	Texture* 	cd;
+};
+
+
+#endif
